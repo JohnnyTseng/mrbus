551c01aa3519961ad161a372b7582cf524ab3136
Mosky <mosky.tw@gmail.com>
2015-01-05 23:44:23 +0800

    added util.debug

--- mrbus/util.py
+++ mrbus/util.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import inspect
 from datetime import datetime
+
+def debug(s):
+    print >> sys.stderr, \
+        'debug:', \
+        '{}:'.format(inspect.currentframe().f_back.f_code.co_name), \
+        s
 
 def get_now_dt():
     return datetime.now()
