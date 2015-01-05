e39b273140d4da0920f725b9dbbb4bc7e9f2f98a
Mosky <mosky.tw@gmail.com>
2015-01-05 23:44:43 +0800

    let psycopg2 return unicode instead of 8-bit string

--- mrbus/conn.py
+++ mrbus/conn.py
@@ -5,5 +5,9 @@ import psycopg2
 from getpass import getuser
 from mosql.db import Database
 
+# let psycopg2 return unicode instead of 8-bit string
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+
 db = Database(psycopg2, user=getuser())
 
