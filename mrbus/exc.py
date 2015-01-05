014dda1ea938383d858a1ed57190d805e33e6b7d
Mosky <mosky.tw@gmail.com>
2015-01-06 00:19:38 +0800

    actually they are value errors

--- mrbus/exc.py
+++ mrbus/exc.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class RouteIDError(KeyError):
+class RouteIDError(ValueError):
     pass
 
-class RouteNameError(KeyError):
+class RouteNameError(ValueError):
     pass
